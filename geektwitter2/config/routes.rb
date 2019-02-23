3602041f403ab449de39d132063c168ffae05388
Takaoooo <takao_lotte7@yahoo.co.jp>
2019-02-23 17:53:11 +0900

    3-8 2週目まで終了

--- geektwitter2/config/routes.rb
+++ geektwitter2/config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
   get '/' => 'hello#index'
   get 'hello/index' => 'hello#index'
   get 'hello/link' => 'hello#link'
+  #Tweetの投稿に関するページ
+  #投稿ページ表示
+  get 'tweets/new' => 'tweets#new'
+  #投稿し、ホームにリダイレクト
+  post 'tweets/new' => 'tweets#create'
+  #投稿一覧
+  get 'tweets/index' => 'tweets#index'
 end
